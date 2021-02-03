ac8467a9d395694e682c85aae73aee335abb0a66
Evelyn199817 <70716664+Evelyn199817@users.noreply.github.com>
2021-02-02 20:53:40 -0600

    Update postwork_s1.R

--- Sesion 1/Postwork/postwork_s1.R
+++ Sesion 1/Postwork/postwork_s1.R
@@ -1,15 +1,15 @@
 library(dplyr)
-
+#datos de soccer de la temporada 2019/2020 de la primera división de la liga española 
 liga.football <- read.csv("https://www.football-data.co.uk/mmz4281/1920/SP1.csv")
 size.registers <- dim(liga.football)[1]
-
+# extrae las columnas que contienen los números de goles anotados por los equipos
 goals.fthg <- liga.football$FTHG
 goals.ftag <- liga.football$FTAG
 
 pm.fthg <- (table(goals.fthg) / size.registers) * 100
 pm.ftag <- (table(goals.ftag) / size.registers) * 100
 pmc <- (table(goals.fthg, goals.ftag) / size.registers) * 100
-
+# tablas de frecuencias relativas para estimar las probabilidades
 print(pm.fthg)
 print(pm.ftag)
 print(pmc)
