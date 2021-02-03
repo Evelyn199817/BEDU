516d498c9668dd8e4e00c3eb79d4e9ad95ae9abb
Evelyn199817 <70716664+Evelyn199817@users.noreply.github.com>
2021-02-02 20:39:03 -0600

    Delete Sesion 1 directory

--- Sesion 1/reto_1_s1.R
+++
@@ -1,8 +0,0 @@
-netflix <- read.csv("https://raw.githubusercontent.com/ecoronadoj/Sesion_1/main/Data/netflix_titles.csv")
-dim(netflix)
-typeof(netflix)
-str(netflix)
-
-net.2015 <- netflix[netflix$release_year > 2015, ]
-write.csv(net.2015, "netflix_release2015")
-
