3d912b004dcbdc0c3594bc77cdd1137d788fdaf0
Evelyn199817 <70716664+Evelyn199817@users.noreply.github.com>
2021-02-02 21:59:45 -0600

    Update postwork_s7.R

--- Sesion 7/Postwork/postwork_s7.R
+++ Sesion 7/Postwork/postwork_s7.R
@@ -1,24 +1,18 @@
-# Postwork Sesión 7
+#Alojar el fichero data.csv en una base de datos llamada match_games, nombrando al collection como match
 
-# Trabajar solo con RStudio el codigo se probo en MacOS
-library(mongolite) # Se carga la biblioteca de mongolite
-
-# Obtenemos el directorio de trabajo actual
+#Cargra la biblioteca de mongolite
+library(mongolite)
+# Directorio de trabajo actual y del proyecto
 currentWD <- getwd()
-
-# Obtenemos el directorio del proyecto
 dirProyect <- dirname(rstudioapi::getSourceEditorContext()$path)
 
-# Verificamos si estamos en el mismo directorio del proyecto
 if (dirProyect != currentWD)
 {
-  setwd(dirProyect) # Ponemos el directorio del proyecto
+  setwd(dirProyect) 
 }
 
-# Obtenemos el directorio donde se guardaran los archivos
+# Directorio donde se guardaran los archivos y el direcorio de trabajo donde se guardaran los archivos
 dirDataFrame <- paste(dirProyect, "/data-postwork7", sep = "")
-
-# Ponemos el directorio de trabajo en la carpeta donde se guardaran nuestro archivos
 setwd(dirDataFrame)
 
 # URL de los datos
@@ -27,21 +21,22 @@ url.data <- "https://raw.githubusercontent.com/beduExpert/Programacion-con-R-San
 # Obtenemos los datos y los almacenamos en los data frames.
 download.file(url = url.data, destfile = "data.csv", mode = "wb")
 
-# Se crea conexion con mongo y se crea la coleccion.
+# Conexion con MONGO para crear una coleccionllamada "match_games"
 mongo_collection = mongo(collection = "match", db = "match_games")
-
-# Insertamos nuestros datos a la base de daatos
 mongo_collection$insert(match)
 
-# Realizar un count para conocer el número de registros que se tiene en la base
+# Count:conocer el número de registros que se tiene en la base
 mongo_collection$count()
 
-# el 20 de diciembre de 2015 y contra que equipo jugó, ¿perdió ó fue goleada?
+
+# realizamos la busqueda del dia 20/12/2015 dentro de la coleccion , sin embargo podemos apreciar que no se encuentran resultados de dicha busqueda
 mongo_collection$find('{"Date":"2015-12-20", "HomeTeam":"Real Madrid"}')
+#Podemos apreciar que dicha base de datos no muestra nungun dato referente al año del 2015, en dicho caso es recomendable el buscar informacion adjunta en una base de datos 
+#diferente que contenga datos de dicho año o hacer un replanteamiento de la pregunta.
 
-# No se encontraron resultados para esa fecha
+#Podemso realizar una busqueda diferente el equipo del Real Madrid
 mongo_collection$find('{"Date":"2017-08-27", "HomeTeam":"Real Madrid"}')
-# En este si se encontro resultados  fue Real Madrid Valencia y fue empate 2-2
+# En este caso se encontro el resultado donde el Real Madrid Valencia tuvo un partidoque termino en empate 2-2
 
-# Se termina la conexion con mongo
+#Terminar la conexion con MONGO 
 rm(mongo_collection)
